891f4dcd121069ad4fd5321547d45d9d58bd442e
Tommy Roberts <roberts.lawrence.1@gmail.com>
2020-09-22 10:31:22 -0500

    Added first build of portfolio site

---
+++ precache-manifest.99dd0906eb15c387410c28674ec0da02.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89629411e27687a00bd3c876be39dabf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72b976524ddc6e05c465",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "b3763f69aab0048b419f",
+    "url": "/static/css/main.fa0919e2.chunk.css"
+  },
+  {
+    "revision": "72b976524ddc6e05c465",
+    "url": "/static/js/2.d62b2fe6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.d62b2fe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3763f69aab0048b419f",
+    "url": "/static/js/main.290fcf17.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "74fe7c8d7bbe165b6e33c5626c05b07d",
+    "url": "/static/media/fisher.74fe7c8d.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "0d3c2534603ad0f51efaaf14c7bd6f35",
+    "url": "/static/media/sockets.0d3c2534.png"
+  },
+  {
+    "revision": "eafe0e31699ef1f4f699fe224b0cd9d3",
+    "url": "/static/media/zbase.eafe0e31.png"
+  }
+]);
