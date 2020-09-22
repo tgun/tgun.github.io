1777a45078386983702ef9951506c0f35536ef77
Tommy Roberts <roberts.lawrence.1@gmail.com>
2020-09-22 13:39:50 -0500

    Add new files

---
+++ precache-manifest.dfc25bcf24e8689292bcdcf0451266cb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b54f8d2841e812353d74ff2c343215f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc909cee270d6509c2e6",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "00ee59f9102fa14b8d29",
+    "url": "/static/css/main.fa0919e2.chunk.css"
+  },
+  {
+    "revision": "bc909cee270d6509c2e6",
+    "url": "/static/js/2.dc0321e1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.dc0321e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00ee59f9102fa14b8d29",
+    "url": "/static/js/main.e418dc5f.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "74fe7c8d7bbe165b6e33c5626c05b07d",
+    "url": "/static/media/fisher.74fe7c8d.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "0d3c2534603ad0f51efaaf14c7bd6f35",
+    "url": "/static/media/sockets.0d3c2534.png"
+  },
+  {
+    "revision": "eafe0e31699ef1f4f699fe224b0cd9d3",
+    "url": "/static/media/zbase.eafe0e31.png"
+  }
+]);
